c068d1a0d3777fdc4f11ba05ea9fb3903c507a61
Iris Kuhn <ichan266@hotmail.com>
2021-06-14 18:45:17 -0700

    tree_folder.py - add explanation to 2 classes

--- tree_folder/tree_folder.py
+++ tree_folder/tree_folder.py
@@ -12,6 +12,7 @@ SPACE_PREFIX = "    "
 
 class DirectoryTree:
     def __init__(self, root_dir):
+        """ Here, we take in root_dir as the root directory as our argument and create an instance attribute, _generator, by using an OOP technique called composition that defines a "has a relationship. This means that every DirectoryTree object has a _TreeGenerator object attached."""
         self._generator = _TreeGenerator(root_dir)
 
     def generate(self):
@@ -26,11 +27,13 @@ class _TreeGenerator:
         self._root_dir = pathlib.Path(root_dir)
         self._tree = []
 
+    def _tree_head(self):
+        """ This method adds _root_dir to the _tree list (from the init method above). It then adds PIPE (see line 6) to the same list, signaling that it's on the top level. `os.sep` probably means starting a new line in CMD"""
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
+
     def build_tree(self):
+        """ In this method, we call the _tree_head method we wrote above to create the tree head, (we haven't written the _tree_body method yet), generate the rest of the file tree diagram by using recursion. """
         self._tree_head()
         self._tree_body(self._root_dir)
         return self._tree
-
-    def _tree_head(self):
-        self._tree.append(f"{self._root_dir}{os.sep}")
-        self._tree.append(PIPE)
